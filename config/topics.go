93ef8c45597ea269e572a7908c13d630ad9ccc77
Anurag <anurag@xrpscan.com>
2024-09-24 17:06:54 +0530

    * Updated Kafka topic name initialization logic

--- config/topics.go
+++ config/topics.go
@@ -2,49 +2,47 @@ package config
 
 import "fmt"
 
-const serviceName = "platform"
-
 // Kafka topic for streaming xrpl.StreamTypeLedger messages
 func TopicLedgers() string {
-	return fmt.Sprintf("%s-%s-ledgers", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-ledgers", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming xrpl.StreamTypeTransaction messages
 func TopicTransactions() string {
-	return fmt.Sprintf("%s-%s-transactions", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-transactions", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming xrpl.StreamTypeValidations messages
 func TopicValidations() string {
-	return fmt.Sprintf("%s-%s-validations", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-validations", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming xrpl.StreamTypeManifests messages
 func TopicManifests() string {
-	return fmt.Sprintf("%s-%s-manifests", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-manifests", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming xrpl.StreamTypePeerStatus messages
 func TopicPeerStatus() string {
-	return fmt.Sprintf("%s-%s-peerstatus", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-peerstatus", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming xrpl.StreamTypeConsensus messages
 func TopicConsensus() string {
-	return fmt.Sprintf("%s-%s-consensus", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-consensus", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming xrpl.StreamTypePathFind messages
 func TopicPathFind() string {
-	return fmt.Sprintf("%s-%s-pathfind", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-pathfind", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming xrpl.StreamTypeServer messages
 func TopicServer() string {
-	return fmt.Sprintf("%s-%s-server", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-server", EnvKafkaTopicNamespace())
 }
 
 // Kafka topic for streaming messages that do not match any StreamType*
 func TopicDefault() string {
-	return fmt.Sprintf("%s-%s-default", EnvKafkaTopicNamespace(), serviceName)
+	return fmt.Sprintf("%s-default", EnvKafkaTopicNamespace())
 }
